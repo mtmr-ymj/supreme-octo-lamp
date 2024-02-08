50384adbbca9b685e84e685f50e4688b81bdc965
mtmr-ymj <mtmr1999@gmail.com>
2024-02-08 23:00:53 +0900

    1-3 新規作成

---
+++ HalloWorld_matsumurayumeji/src/sample/Sample.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo,World");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ HalloWorld_matsumurayumeji/src/sample/SelfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		//氏名
+		String name = "松村　夢二";
+		//年齢
+		int age = 24;
+		//趣味
+		String hobby = "映画鑑賞";
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
